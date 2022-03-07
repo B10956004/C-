ff908165bfb6db39d3bf889f689ddda691d48262
aecd0258 <aecd0258@gmail.com>
2022-03-07 10:42:21 +0800

    upload

---
+++ 練習1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 練習1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            String text = "我只是文字";
+
+            Console.WriteLine(text);
+        }
+    }
+}
