18cee0ef14c21c99863968914fee05ba747dfa6b
B10956004 <84553464+B10956004@users.noreply.github.com>
2022-03-14 10:25:55 +0800

    Add files via upload

---
+++ 0314/0314/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _0314
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("請輸入數字");
+            num=Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Weird");
+            }
+            else if (num / 2 >= 2 && num / 2 <= 5)
+            {
+                Console.WriteLine("Not Weird");
+            }
+            else if(num/2>=6 && num / 2 <= 20)
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+
+        }
+    }
+}
