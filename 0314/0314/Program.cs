457020f7bcf07ffb31ddaae7d25b794d85265321
B10956004 <B10956004@mail.npust.edu.tw>
2022-03-14 10:53:33 +0800

    none

--- 0314/0314/Program.cs
+++ 0314/0314/Program.cs
@@ -6,27 +6,37 @@ namespace _0314
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            Console.WriteLine("請輸入數字");
-            num=Convert.ToInt32(Console.ReadLine());
-            if (num % 2 == 1)
+            //int num = 0;
+            //Console.WriteLine("請輸入數字");
+            //num = Convert.ToInt32(Console.ReadLine());
+            //if (num % 2 == 1)
+            //{
+            //    Console.WriteLine("Weird");
+            //}
+            //else if (num / 2 >= 2 && num / 2 <= 5)
+            //{
+            //    Console.WriteLine("Not Weird");
+            //}
+            //else if (num / 2 >= 6 && num / 2 <= 20)
+            //{
+            //    Console.WriteLine("Weird");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Not Weird");
+            //}
+
+            int n = 0;
+            Console.WriteLine("請輸入N:");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < n; i++)
             {
-                Console.WriteLine("Weird");
-            }
-            else if (num / 2 >= 2 && num / 2 <= 5)
-            {
-                Console.WriteLine("Not Weird");
-            }
-            else if(num/2>=6 && num / 2 <= 20)
-            {
-                Console.WriteLine("Weird");
-            }
-            else
-            {
-                Console.WriteLine("Not Weird");
+                for(int j = 0; j < i; j++)
+                {
+                    Console.WriteLine("*");
+                }
             }
 
-            
         }
     }
 }
