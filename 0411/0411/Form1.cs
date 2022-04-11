09a011cbb23d76f3539acd6bdeb4c52b341079f7
user <user@DESKTOP-6JBEF5K>
2022-04-11 09:58:26 +0800

    none

---
+++ 0411/0411/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.VisualBasic.PowerPacks;
+
+namespace _0411
+{
+    public partial class Form1 : Form
+    {
+        Thread Th;
+        UdpClient U;
+
+        ShapeContainer C;//NUget 類似於pip install
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Listen()
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Th.Abort();
+                U.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnConnent_Click(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Th = new Thread(Listen);
+            Th.Start();
+            btnConnent.Enabled = false;
+        }
+    }
+}
